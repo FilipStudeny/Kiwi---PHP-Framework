834819df70f3809d9c34740dd2143c32fb0b3c45
Filip Studený <BluesyPompanno@seznam.cz>
2022-11-30 10:48:33 +0100

    First files, not working

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+    include_once './core/Request.php';
+    include_once './core/Router.php';
+
+    $router = new Router(new Request);
+
+    $router->get('/', function() {
+      return <<<HTML
+      <h1>Hello world</h1>
+    HTML;
+    });
+    
+    
+    $router->get('/profile', function($request) {
+      return <<<HTML
+      <h1>Profile</h1>
+    HTML;
+    });
+    
+    $router->post('/data', function($request) {
+    
+      return json_encode($request->getBody());
+    });
+?>
